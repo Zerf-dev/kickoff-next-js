bbec62254922323f9499afd79bf2bb49cf9330f0
willian <willianramirez33@gmail.com>
2023-05-19 20:48:19 -0300

    fix: 🐛 Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
-  extends: ['next/core-web-vitals'],
+  extends: ['next/core-web-vitals', 'next'],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
-    'no-console': 'error'
+    'no-console': 'error',
+    'no-unused-vars': 'error'
   }
 };
